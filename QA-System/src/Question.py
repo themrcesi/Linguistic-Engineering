a18984321aa93e12353e14c33b44a077285d1a29
gabarre97 <alejangaba@gmail.com>
2021-01-02 13:37:54 +0100

    Add some rules

--- QA-System/src/Question.py
+++ QA-System/src/Question.py
@@ -19,6 +19,119 @@ class TitleQuestion(Question):
 
     def answer(self):
         self.answer = self.film["título original"]
+        
+class PremiereQuestion(Question):
+    def __init__(self, question, num_rule, numeric_year = None):
+        super().__init__(question)
+        self.num_rule = num_rule
+        self.numeric_year = numeric_year
+    
+    def answer(self):
+        if self.num_rule == 0:
+            self.answer = self.film["año"]
+        elif self.num_rule == 1:
+            self.answer = "Si" if self.film["año"] == self.numeric_year else "No"
+        elif self.num_rule == 2:
+            self.answer = "Si" if int(self.film["año"]) < int(self.numeric_year) else "No"
+        elif self.num_rule == 3:
+            self.answer = "Si" if int(self.film["año"]) > int(self.numeric_year) else "No"
+            
+class DurationQuestion(Question):
+    def __init__(self, question, num_rule, minutes = None):
+        super().__init__(question)
+        self.num_rule = num_rule
+        self.minutes = minutes
+    
+    def answer(self):
+        if self.num_rule == 0:
+            self.answer = self.film["duración"]
+        elif self.num_rule == 1:
+            self.answer = "Si" if int(self.film["duración"].split(' ')[0]) > int(self.minutes) else "No"
+        elif self.num_rule == 2:
+            self.answer = "Si" if int(self.film["duración"].split(' ')[0]) < int(self.minutes) else "No"
+            
+class DirectionQuestion(Question):
+    def __init__(self, question, num_rule, director = None):
+        super().__init__(question)
+        self.num_rule = num_rule
+        self.director = director
+    
+    def answer(self):
+        if self.num_rule == 0:
+            self.answer = self.film["dirección"]
+        elif self.num_rule == 1:
+            self.answer = "Si" if self.director in self.film["dirección"].lower() else "No"
+            
+class ScreenwritterQuestion(Question):
+    def __init__(self, question, num_rule, screenwritter = None):
+        super().__init__(question)
+        self.num_rule = num_rule
+        self.screenwritter = screenwritter
+    
+    def answer(self):
+        if self.num_rule == 0:
+            self.answer = self.film["guión"]
+        elif self.num_rule == 1:
+            self.answer = "Si" if self.screenwritter in self.film["guión"].lower() else "No"
+        elif self.num_rule == 2:
+            self.answer = len(self.film["guión"].split(','))
+            
+class MusicQuestion(Question):
+
+    def answer(self):
+        self.answer = self.film["música"]
+        
+class CastQuestion(Question):
+    def __init__(self, question, num_rule, principal_actors = 3, actor = None):
+        super().__init__(question)
+        self.num_rule = num_rule
+        self.actor = actor
+        self.principal_actors = principal_actors
+    
+    def answer(self):
+        if self.num_rule == 0:
+            self.answer = self.film["reparto"]
+        elif self.num_rule == 1:
+            all_actors = self.film["reparto"].split(',')
+            if len(all_actors) < self.principal_actors:
+                self.principal_actors = len(all_actors)
+            self.answer = ','.join(all_actors[:self.principal_actors])
+        elif self.num_rule == 2:
+            self.answer = "Si" if self.actor in self.film["reparto"].lower() else "No"
+            
+class ProducerQuestion(Question):
+
+    def answer(self):
+        self.answer = self.film["productora"]
+        
+class GenderQuestion(Question):
+    def __init__(self, question, num_rule, gender = None):
+        super().__init__(question)
+        self.num_rule = num_rule
+        self.gender = gender
+    
+    def answer(self):
+        if self.num_rule == 0:
+            self.answer = self.film["género"]
+        elif self.num_rule == 1:
+            self.answer = "Si" if self.gender in self.film["género"].lower() else "No"
+            
+class GroupQuestion(Question):
+    def __init__(self, question, num_rule, saga = None):
+        super().__init__(question)
+        self.num_rule = num_rule
+        self.saga = saga
+    
+    def answer(self):
+        if self.num_rule == 0:
+            self.answer = self.film["grupos"]
+        elif self.num_rule == 1:
+            self.answer = "Si" if self.saga in self.film["grupos"].lower() else "No"
+            
+class SynopsisQuestion(Question):
+
+    def answer(self):
+        self.answer = self.film["sinopsis"]
 
 class UnknownQuestion(Question):
 
--- QA-System/src/QuestionParser.py
+++ QA-System/src/QuestionParser.py
@@ -1,6 +1,25 @@
 from DocumentProcessor import PreprocessorDocument
-from Question import TitleQuestion, UnknownQuestion
+from Question import (TitleQuestion, UnknownQuestion, PremiereQuestion, DurationQuestion, 
+                      DirectionQuestion, ScreenwritterQuestion, MusicQuestion, CastQuestion,
+                      ProducerQuestion, GenderQuestion, GroupQuestion, SynopsisQuestion)
+import re
 
+year_regex = 'en (el año)? \d{4}$'
+
+before_year_regex = 'antes de (el año)? \d{4}$'
+after_year_regex = '(después|despues) de (el año)? \d{4}$'
+
+duration_regex = '[0-9]+'
+
+direction_regex = '(dirigio|dirigió) (\w+)'
+
+screenwritter_regex = '(\w+) el guionista'
+
+category_regex = '(genero|género) (\w+)'
+
+actor_regex = '(participó|participo|actuó|actuo) (\w+)'
+
+saga_regex = 'saga ([^.]+|\S+)'
 class Parser():
 
     # Path for the films
@@ -16,36 +35,96 @@ class Parser():
 
     def parse(self):
         q = self.question.lower()
-
         # TÍTULO
-        if ("cuál" in q or "cual" in q) and "original" in q and ("nombre" in q or "título" in q):
+        if ("cuál" in q or "cual" in q) and "original" in q and ("nombre" in q or "título" in q or "titulo" in q):
             quest = TitleQuestion(q)
         # ESTRENO
-
+        elif ("cuándo" in q or "cuando" in q) and ("se estrenó" in q or "se estreno" in q or "salió" in q or "salio" in q):
+            quest = PremiereQuestion(q,0)
+            
+        elif ("se estrenó" in q or "se estreno" in q or "salió" in q or "salio" in q) \
+                and re.findall(year_regex, q) != []:
+            quest = PremiereQuestion(q,1, re.findall(year_regex, q)[0].split(' ')[-1])
+            
+        elif ("se estrenó" in q or "se estreno" in q or "salió" in q or "salio" in q) \
+                and re.findall(before_year_regex, q) != []:
+            quest = PremiereQuestion(q, 2, re.findall(before_year_regex, q)[0].split(' ')[-1])
+            
+        elif ("se estrenó" in q or "se estreno" in q or "salió" in q or "salio" in q) \
+                and re.findall(after_year_regex, q) != []:
+            quest = PremiereQuestion(q, 3, re.findall(after_year_regex, q)[0][1].split(' ')[-1])
+            
         # DURACIÓN
+        
+        elif ("cuánto" in q or "cuanto" in q) and "dura" in q:
+            quest = DurationQuestion(q, 0)
+            
+        elif "dura" in q and ("más de" in q or "mas de" in q) and re.findall(duration_regex,q) != [] and "minutos" in q:
+            quest = DurationQuestion(q, 1, re.findall(duration_regex,q)[0])
+            
+        elif "dura" in q and ("más de" in q or "menos de" in q) and re.findall(duration_regex,q) != [] and "minutos" in q:
+            quest = DurationQuestion(q, 2, re.findall(duration_regex,q)[0])
 
         # DIRECCIÓN
-
+        elif ("quién" in q or "quien" in q) and ("dirig" in q or "director" in q):
+            quest = DirectionQuestion(q, 0)
+            
+        elif re.findall(direction_regex,q) != []:
+            quest = DirectionQuestion(q, 1, re.findall(direction_regex,q)[0][1])
         # GUIÓN
+        elif ("quién" in q or "quien" in q) and "guionista" in q:
+            quest = ScreenwritterQuestion(q, 0)
+            
+        elif ("fue" in q or "es" in q or "ha sido" in q) and re.findall(screenwritter_regex,q) != []: #Pendiente
+            quest = ScreenwritterQuestion(q, 1, re.findall(screenwritter_regex,q)[0])
+            
+        elif ("cuántos" in q or "cuantos" in q) and "guionista" in q:
+            quest = ScreenwritterQuestion(q, 2)
 
         # MÚSICA
+        elif ("quién" in q or "quien" in q) and "compuso" in q and "banda sonora" in q:
+            quest = MusicQuestion(q)
 
         # REPARTO
+        elif ("qué" in q or "que" in q) and ("actor" in q or "actri" in q) and \
+            ("tiene" in q or "particip" in q or "actua" in q):
+            quest = CastQuestion(q, 0)
+        
+        elif ("quienes" in q or "quiénes" in q) and re.findall(duration_regex,q) != [] and "actores principales" in q:
+            num_actors = int(re.findall(duration_regex,q)[0])
+            quest = CastQuestion(q, 1, principal_actors = num_actors)
 
+        elif re.findall(actor_regex,q) != []:
+            actor = re.findall(actor_regex,q)[0][1]
+            quest = CastQuestion(q, 2, actor = actor)
+            
         # PRODUCTORA
-
+        elif ("cuál" in q or "cual" in q) and "productora" in q:
+            quest = ProducerQuestion(q)
         # GÉNERO
+        elif ("qué" in q or "que" in q) and ("genero" in q or "género" in q or "categoría" in q or "categoria" in q):
+            quest = GenderQuestion(q, 0)
+            
+        elif ("fue" in q or "es" in q or "ha sido" in q) and re.findall(category_regex,q) != []:
+            gender = re.findall(category_regex,q)[0][1].split(' ')[-1]
+            quest = GenderQuestion(q, 1, gender)
 
         # GRUPOS
+        elif ("qué" in q or "que" in q) and "saga" in q:
+            quest = GroupQuestion(q, 0)
+        elif ("pertenece" in q or "es" in q) and re.findall(saga_regex,q) != []:
+            saga = re.findall(saga_regex,q)[0]
+            quest = GroupQuestion(q, 1, saga)
 
         # SINOPSIS
-
+        elif ("cuál" in q or "cual" in q) and ("resumen" in q or "sinopsis" in q):
+            quest = SynopsisQuestion(q)
         # VALORACIÓN
 
         # PREMIOS
 
         # CRÍTICAS
-
+        
         # OTRA
         else:
             quest = UnknownQuestion(q)
